be424066b23f227c4d738e980bed0ba533bc5725
unknown <raavendanp@eafit.edu.co>
2020-11-08 23:09:06 -0500

    correccion

--- app/Util/ImageLocalStorage.php
+++ app/Util/ImageLocalStorage.php
@@ -19,4 +19,16 @@ class ImageLocalStorage implements ImageStorage
         Storage::disk('public')->delete('test'.$pid.'.png');   
     }
 
+    public function stores3($request, $pid)
+    {
+        
+        if ($request->hasFile('product_image')) {
+            Storage::disk('s3')->put("test".$pid.".png", file_get_contents($request->file('product_image')->getRealPath()));
+        }
+    }
+    public function deletes3($pid)
+    {
+        Storage::disk('s3')->delete('test'.$pid.'.png');   
+    }
+
 }
