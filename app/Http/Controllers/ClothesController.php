297313f973ebf4795a28a72fba0764f7fc2fc03e
unknown <raavendanp@eafit.edu.co>
2020-11-10 22:12:51 -0500

    .env

--- app/Http/Controllers/ClothesController.php
+++ app/Http/Controllers/ClothesController.php
@@ -10,7 +10,7 @@ class ClothesController extends Controller
 {
     public function show()
     {
-        $api= Http::get('http://helize.tk/public/api/wears');
+        $api= Http::get('http://3.217.114.44/public/api/v3/products');
         $japi = $api->json();
         return view('clothes.showClothes', compact('japi'));
     }
